1c79ce5a044e4b055ff5b6a3509ddc83932e717c
ian <ian@coinroutes.com>
2021-10-22 00:47:32 -0400

    added VNA code

--- radar/sweep.py
+++ radar/sweep.py
@@ -1,65 +1,103 @@
 #!/usr/bin/env python3
+import argparse
 
 import time
 import csv
 from libreVNA import libreVNA
 import datetime
+import os
 
-# Create the control instance
-vna = libreVNA('localhost', 19542)
+class VNAGPR(object):
+    calibration = os.environ.get('CALIBRATION', '/Users/ian/projects/1-6g.cal')
+    def __init__(self):
+        pass
 
-# Quick connection check (should print "LibreVNA-GUI")
-print(vna.query("*IDN?"))
+    def connect(self):
+        # Create the control instance
+        self.vna = libreVNA('localhost', 19542)
 
-# Make sure we are connecting to a device (just to be sure, with default settings the LibreVNA-GUI auto-connects)
-vna.cmd(":DEV:CONN")
-dev = vna.query(":DEV:CONN?")
-if dev == "Not connected":
-    print("Not connected to any device, aborting")
-    exit(-1)
-else:
-    print("Connected to "+dev)
+        # Quick connection check (should print "LibreVNA-GUI")
+        print(self.vna.query("*IDN?"))
 
-# Simple trace data extraction
+        # Make sure we are connecting to a device (just to be sure, with default settings the LibreVNA-GUI auto-connects)
+        self.vna.cmd(":DEV:CONN")
+        dev = self.vna.query(":DEV:CONN?")
+        if dev == "Not connected":
+            print("Not connected to any device, aborting")
+            exit(-1)
+        else:
+            print("Connected to "+dev)
 
-# switch to VNA mode, setup the sweep parameters
-print("Setting up the sweep...")
-vna.cmd(":DEV:MODE VNA")
-vna.cmd(":VNA:SWEEP FREQUENCY")
-vna.cmd(":VNA:STIM:LVL -10")
-vna.cmd(":VNA:ACQ:IFBW 10000")
-vna.cmd(":VNA:ACQ:AVG 1")
-vna.cmd(":VNA:ACQ:POINTS 101")
-vna.cmd(":VNA:AQC 1")
-#vna.cmd(":VNA:AQQuisition:AVG 1")
-#vna.cmd(":VNA:FREQuency:SPAN 1")
-vna.cmd(":VNA:FREQuency:START 1000000000")
-vna.cmd(":VNA:FREQuency:STOP 2000000000")
+    def scan(self):
+        vna = self.vna
+        # Simple trace data extraction
 
-# wait for the sweep to finish
-print("Waiting for the sweep to finish...")
-while vna.query(":VNA:ACQ:FIN?") == "FALSE":
-    time.sleep(0.1)
+        # switch to VNA mode, setup the sweep parameters
+        print("Setting up the sweep...")
+        vna.cmd(":DEV:MODE VNA")
+        vna.cmd(":VNA_CAL:LOAD {}".format(self.calibration))
+        vna.cmd(":VNA:SWEEP FREQUENCY")
+        vna.cmd(":VNA:STIM:LVL 0")
+        vna.cmd(":VNA:ACQ:IFBW 10000")
+        vna.cmd(":VNA:ACQ:AVG 1")
+        vna.cmd(":VNA:ACQ:POINTS 101")
+        vna.cmd(":VNA:AQC 1")
+        #vna.cmd(":VNA:AQQuisition:AVG 1")
+        #vna.cmd(":VNA:FREQuency:SPAN 1")
+        vna.cmd(":VNA:FREQuency:START 1000000000")
+        vna.cmd(":VNA:FREQuency:STOP 2000000000")
 
-# grab the data of trace S11
-print("Reading trace data...")
+        # wait for the sweep to finish
+        print("Waiting for the sweep to finish...")
+        while vna.query(":VNA:ACQ:FIN?") == "FALSE":
+            time.sleep(0.1)
 
-while True:
-    start = datetime.datetime.utcnow()
-    data = vna.query(":VNA:TRACE:DATA? S21")
-    end = datetime.datetime.utcnow()
-    print("took {} seconds".format((end - start).total_seconds()))
-    S21 = vna.parse_trace_data(data)
-    with open('data/{}'.format(start.isoformat()),'w') as e:
-        writer = csv.writer(e)
+    def writedata(self, output):
 
-        for row in S21:
-            freq, real, imag = row[0], row[1].real, row[1].imag
-            writer.writerow((freq,real,imag))
-    time.sleep(0.025)
-# Returned data is just a string containing all the measurement points.
-# Parsing the data returns a list containing frequency/complex tuples
+        # grab the data of trace S11
+        print("Reading trace data...")
+        directory = 'data/{}'.format(output)
+        if os.path.exists(directory):
+            raise Exception("Directory exists")
+
+        os.makedirs(directory)
+        while True:
+            start = datetime.datetime.utcnow()
+            data = self.vna.query(":VNA:TRACE:DATA? S21")
+            end = datetime.datetime.utcnow()
+            print("took {} seconds".format((end - start).total_seconds()))
+            S21 = self.vna.parse_trace_data(data)
+            print(output)
 
 
-for x in S11:
-    print(x)
+            with open('{}/{}'.format(directory, start.isoformat()),'w') as e:
+                writer = csv.writer(e)
+
+                for row in S21:
+                    freq, real, imag = row[0], row[1].real, row[1].imag
+                    writer.writerow((freq,real,imag))
+            time.sleep(0.025)
+
+    def run(self):
+        self.connect()
+        self.scan()
+
+
+def main():
+    parser = argparse.ArgumentParser(
+    description=__doc__,
+    formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument("-o", "--output", type=str,
+                    help="ouput file name for DZT, .DZT will be added")
+
+
+    args = parser.parse_args()
+    output = args.output
+    GPR = VNAGPR()
+
+    GPR.run()
+    GPR.writedata(args.output)
+
+if __name__=="__main__":
+    main()
