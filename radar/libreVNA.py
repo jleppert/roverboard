7c35aeb97bc491367a705bc9df83bd75ebaf2988
ian <ian@coinroutes.com>
2021-10-22 19:45:31 -0400

    added asyncio librevna

--- radar/libreVNA.py
+++ radar/libreVNA.py
@@ -1,5 +1,9 @@
 import socket
+import logging
 from asyncio import IncompleteReadError  # only import the exception class
+import asyncio
+
+logger = logging.getLogger(__name__)
 
 class SocketStreamReader:
     def __init__(self, sock: socket.socket):
@@ -26,7 +30,8 @@ class SocketStreamReader:
         if len(separator) != 1:
             raise ValueError("Only separators of length 1 are supported.")
 
-        chunk = bytearray(4096)
+        #chunk = bytearray(4096)
+        chunk = bytearray(16384)
         start = 0
         buf = bytearray(len(self._recv_buffer))
         bytes_read = self._recv_into(memoryview(buf))
@@ -80,6 +85,59 @@ class libreVNA:
         self.sock.sendall(query.encode())
         self.sock.send(b"\n")
         return self.__read_response()
+
+    @staticmethod
+    def parse_trace_data(data):
+        ret = []
+        # Remove brackets (order of data implicitly known)
+        data = data.replace(']','').replace('[','')
+        values = data.split(',')
+        if int(len(values) / 3) * 3 != len(values):
+            # number of values must be a multiple of three (frequency, real, imaginary)
+            raise Exception("Invalid input data: expected tuples of three values each")
+        for i in range(0, len(values), 3):
+            freq = float(values[i])
+            real = float(values[i+1])
+            imag = float(values[i+2])
+            ret.append((freq, complex(real, imag)))
+        return ret
+
+
+
+class libreVNAAsync:
+    def __init__(self, host='localhost', port=19542):
+        self.host = host
+        self.port = port
+
+    async def connect(self):
+        try:
+            self.reader, self.writer = await asyncio.open_connection(self.host, self.port)
+        except:
+            logger.exception("Failed to connect")
+            raise Exception("Unable to connect to LibreVNA-GUI. Make sure it is running and the TCP server is enabled.")
+
+    async def close(self):
+        self.writer.close()
+        try:
+            await self.writer.wait_closed()
+        except:
+            print("python doesn't support wait_Closed, sleeping")
+            await asyncio.sleep(0.1)
+
+
+    async def __read_response(self):
+        data = await self.reader.readline()
+        return data.decode().rstrip()
+
+    async def cmd(self, cmd):
+        self.writer.write(cmd.encode() + b"\n")
+        await self.writer.drain()
+        return await self.__read_response()
+
+    async def query(self, query):
+        self.writer.write(query.encode() + b"\n")
+        await self.writer.drain()
+        return await self.__read_response()
 
     @staticmethod
     def parse_trace_data(data):
--- radar/sweep.py
+++ radar/sweep.py
@@ -5,10 +5,10 @@ import concurrent.futures
 import time
 import csv
 try:
-    from .libreVNA import libreVNA
+    from .libreVNA import libreVNA, libreVNAAsync
     from .tdr import TDR
 except:
-    from libreVNA import libreVNA
+    from libreVNA import libreVNA, libreVNAAsync
     from tdr import TDR
 
 import datetime
@@ -25,6 +25,7 @@ class VNAGPR(object):
 
     def connect(self):
         # Create the control instance
+
         self.vna = libreVNA('localhost', 19542)
 
         # Quick connection check (should print "LibreVNA-GUI")
@@ -154,6 +155,115 @@ class VNAGPR(object):
         self.scan()
 
 
+class VNAGPRAsync(object):
+    calibration = os.environ.get('CALIBRATION', '/Users/ian/projects/SOLT 1.00M-3.00G 303pt.cal')
+
+    vna = None
+
+    def __init__(self):
+        pass
+
+    async def connect(self):
+        # Create the control instance
+
+        self.vna = libreVNAAsync('localhost', 19542)
+        await self.vna.connect()
+
+        # Quick connection check (should print "LibreVNA-GUI")
+        print(await self.vna.query("*IDN?"))
+
+        # Make sure we are connecting to a device (just to be sure, with default settings the LibreVNA-GUI auto-connects)
+        await self.vna.cmd(":DEV:CONN")
+        dev = await self.vna.query(":DEV:CONN?")
+        if dev == "Not connected":
+            print("Not connected to any device, aborting")
+            exit(-1)
+        else:
+            print("Connected to "+dev)
+
+    async def scan(self):
+        # Simple trace data extraction
+
+        # switch to VNA mode, setup the sweep parameters
+        print("Setting up the sweep...")
+        await self.vna.cmd(":DEV:MODE VNA")
+        await self.vna.cmd(":VNA_CAL:LOAD {}".format(self.calibration))
+        await self.vna.cmd(":VNA_CAL:TYPE SOLT")
+        await self.vna.cmd(":VNA:SWEEP FREQUENCY")
+        await self.vna.cmd(":VNA:STIM:LVL 0")
+        await self.vna.cmd(":VNA:ACQ:IFBW 10000")
+        await self.vna.cmd(":VNA:ACQ:AVG 1")
+        await self.vna.cmd(":VNA:ACQ:POINTS 303")
+        await self.vna.cmd(":VNA:AQC 1")
+        #vna.cmd(":VNA:AQQuisition:AVG 1")
+        #vna.cmd(":VNA:FREQuency:SPAN 1")
+        await self.vna.cmd(":VNA:FREQuency:START 10000000")
+        await self.vna.cmd(":VNA:FREQuency:STOP 3000000000")
+
+        # wait for the sweep to finish
+        print("Waiting for the sweep to finish...")
+        while await self.vna.query(":VNA:ACQ:FIN?") == "FALSE":
+            await asyncio.sleep(0.1)
+
+    async def close(self):
+        if self.vna:
+            return await self.vna.close()
+
+    async def writedata_async(self, output, run_seconds=None):
+
+        if self.vna is None:
+            await self.connect()
+            await self.scan()
+
+        loop = asyncio.get_event_loop()
+
+        # grab the data of trace S11
+        print("Reading trace data...")
+        self.directory = 'data/{}'.format(output)
+        if os.path.exists(self.directory):
+            raise Exception("Directory exists")
+
+        os.makedirs(self.directory)
+        start_time = datetime.datetime.utcnow()
+
+        tasks = []
+        with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+
+
+            while True:
+                start = datetime.datetime.utcnow()
+                data = await self.vna.query(":VNA:TRACE:DATA? S21")
+                end = datetime.datetime.utcnow()
+                total_seconds = (end - start).total_seconds()
+                print("took {} seconds".format(total_seconds))
+                S21 = self.vna.parse_trace_data(data)
+                print(output)
+
+                def write_file():
+                    with open('{}/{}'.format(self.directory, start.isoformat()),'w') as e:
+                        writer = csv.writer(e)
+
+                        for row in S21:
+                            freq, real, imag = row[0], row[1].real, row[1].imag
+                            writer.writerow((freq,real,imag))
+
+                tasks.append(loop.run_in_executor(executor, write_file))
+
+                time_ran = datetime.datetime.utcnow() - start_time
+                if total_seconds < 0.01:
+                    await asyncio.sleep(0.01)
+
+                if run_seconds and time_ran.total_seconds() > run_seconds:
+                    print("running for {} seconds, stopping capture".format(run_seconds))
+                    break
+            await asyncio.gather(*tasks)
+
+
+    async def run(self):
+        await self.connect()
+        await self.scan()
+
+
 def main():
     parser = argparse.ArgumentParser(
     description=__doc__,
@@ -174,14 +284,16 @@ def main():
     args = parser.parse_args()
     output = args.output
     try:
-        GPR = VNAGPR()
-
-        GPR.run()
 
         if not args.async:
             print("not using async")
+            GPR = VNAGPR()
+
+            GPR.run()
             GPR.writedata(args.output, args.time)
         else:
+            print("using async")
+            GPR = VNAGPRAsync()
             asyncio.set_event_loop(asyncio.new_event_loop())
             loop = asyncio.get_event_loop()
             loop.run_until_complete(GPR.writedata_async(args.output, args.time))
@@ -191,7 +303,10 @@ def main():
             tdr = TDR(use_csv=True)
             tdr.listFolder(GPR.directory + '/', GPR.directory +'o')
     finally:
-        GPR.close()
+        if not args.async:
+            GPR.close()
+        else:
+            loop.run_until_complete(GPR.close())
 
 
 if __name__=="__main__":
